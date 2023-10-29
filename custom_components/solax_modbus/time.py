63fb1084e49f7f72c24e1a16a7992cfdcad80820
wills106 <william.f.swann@googlemail.com>
2023-10-29 12:35:26 +0000

    time.py - not working
    Populates a time entity, but nothing happens when you set a time.

--- custom_components/solax_modbus/time.py
+++ custom_components/solax_modbus/time.py
@@ -1,4 +1,5 @@
 #from __future__ import annotations
+from pickle import NONE
 from .const import ATTR_MANUFACTURER, DOMAIN, CONF_MODBUS_ADDR, DEFAULT_MODBUS_ADDR
 from .const import WRITE_DATA_LOCAL, WRITE_MULTISINGLE_MODBUS, WRITE_SINGLE_MODBUS
 from homeassistant.components.time import TimeEntity, TimeEntityDescription
@@ -6,6 +7,7 @@ from homeassistant.util import slugify
 from homeassistant.const import CONF_NAME
 from homeassistant.core import callback
 from datetime import datetime, time
+from typing import Any, Dict, Optional
 import logging
 
 _LOGGER = logging.getLogger(__name__)
@@ -53,6 +55,7 @@ class SolaXModbusTime(TimeEntity):
                  modbus_addr,
                  device_info,
                  time_info
+
     ) -> None:
         """Initialize the time."""
         self._platform_name = platform_name
@@ -65,6 +68,7 @@ class SolaXModbusTime(TimeEntity):
         self._option_dict = time_info.option_dict
         self.entity_description = time_info
         self._write_method = time_info.write_method
+        self._attr_native_value = None
 
     async def async_added_to_hass(self):
         """Register callbacks."""
@@ -91,30 +95,27 @@ class SolaXModbusTime(TimeEntity):
     def unique_id(self) -> Optional[str]:
         return f"{self._platform_name}_{self._key}"
 
-    @property
-    def native_value(self) -> time | None:
-        return datetime.now()
-    #    """Get native value."""
-    #    #delta = timedelta
-    #    return datetime.time()
+    #@property
+    #def native_value(self) -> time | None:
+    #    """Return the value reported by the time."""
+    #    return time(self.entity_description).isoformat
 
-    async def async_set_value(self, value: time) -> None:
-        """Set native value."""
-        payload = value
+    def set_value(self, value: time) -> None:
+        """Change the time."""
+        #setattr(self._option_dict, self.entity_description.key, value)
+        return self._attr_native_value
+
+    async def async_select_option(self, value: time) -> None:
+        """Change the select option."""
+        payload = get_payload(self._option_dict, value)
         if self._write_method == WRITE_MULTISINGLE_MODBUS:
-            _LOGGER.info(f"writing {self._platform_name} {self._key} number register {self._register} value {payload} after div by readscale {self.entity_description.read_scale} scale {self._attr_scale}")
+            _LOGGER.info(f"writing {self._platform_name} select register {self._register} value {payload}")
             self._hub.write_registers_single(unit=self._modbus_addr, address=self._register, payload=payload)
         elif self._write_method == WRITE_SINGLE_MODBUS:
-            _LOGGER.info(f"writing {self._platform_name} {self._key} number register {self._register} value {payload} after div by readscale {self.entity_description.read_scale} scale {self._attr_scale}")
+            _LOGGER.info(f"writing {self._platform_name} select register {self._register} value {payload}")
             self._hub.write_register(unit=self._modbus_addr, address=self._register, payload=payload)
         elif self._write_method == WRITE_DATA_LOCAL:
             _LOGGER.info(f"*** local data written {self._key}: {payload}")
-            #corresponding_sensor = self._hub.preventSensors.get(self.entity_description.key, None)
-            if self.entity_description.prevent_update: # if corresponding_sensor: # only if corresponding sensor has prevent_update=True
-                self._hub.tmpdata[self.entity_description.key] = payload
-                self._hub.tmpdata_expiry[self.entity_description.key] = time() + TMPDATA_EXPIRY
-                # corresponding_sensor.async_write_ha_state()
             self._hub.localsUpdated = True # mark to save permanently
-        self._hub.data[self._key] = value/self.entity_description.read_scale
-        #_LOGGER.info(f"*** data written part 2 {self._key}: {self._hub.data[self._key]}")
-        self.async_write_ha_state() # is this needed ?
+        self._hub.data[self._key] = value
+        self.async_write_ha_state()
